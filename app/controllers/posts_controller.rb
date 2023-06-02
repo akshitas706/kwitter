53612e15d659b87abe7892e2f7df4398a7d51312
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-06-02 19:50:48 +0000

    ssrget

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,22 @@ class PostsController < ApplicationController
 
   def index 
     @posts = Post.all.order("created_at desc")
+    posts_json = []
+
+    for posts in @posts
+      user = User.find_by({"id" => post["user_id"]})
+      posts_json << {
+      "username" => ...,
+      "real_name" => ...,
+      "body" => post ["body"],
+      "created_at" =>["created_at"]
+    }
   end
+
+respond_to do [format]
+  format.html
+  format.json { render : json => posts_json}
+end
 
   def create
     post = Post.new
